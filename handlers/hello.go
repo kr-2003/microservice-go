8a2f292b47e688efda10e18aa86bee8e2f825eeb
kr-2003 <kumar.kr.abhinav@gmail.com>
2023-05-17 15:03:10 +0530

    again: again

---
+++ handlers/hello.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "fmt"
+
+func SayHello(){
+	fmt.Printf("hello from handler")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/app/handlers"
+)
+
+func main() {
+	fmt.Printf("Hello world")
+	handlers.SayHello()
+}
